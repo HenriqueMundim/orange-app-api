2009c7a61fd2907fed9d768d4c94ba37ea59817a
Henrique Matos <henriquemundimmatos@gmail.com>
2024-07-24 09:18:09 -0300

    feat: Create validate token method

--- src/main/java/com/HenriqueMundim/github/com/orange_app_api/domain/services/token/TokenService.java
+++ src/main/java/com/HenriqueMundim/github/com/orange_app_api/domain/services/token/TokenService.java
@@ -4,6 +4,7 @@ import com.HenriqueMundim.github.com.orange_app_api.domain.entities.User;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -23,6 +24,7 @@ public class TokenService {
 
             return JWT.create()
                     .withIssuer("orange-app")
+                    .withSubject(user.getUsername())
                     .withExpiresAt(generateExpirationDate())
                     .sign(algorithm);
 
@@ -32,6 +34,21 @@ public class TokenService {
         }
     }
 
+    public String validateToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(tokenSecret);
+
+            return JWT.require(algorithm)
+                    .withIssuer("orange-app")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+
+        } catch (JWTVerificationException exception) {
+            System.out.println(exception.getMessage());
+            return "";
+        }
+    }
 
     private Instant generateExpirationDate() {
         return LocalDateTime.now().plusHours(1).toInstant(ZoneOffset.of("-3:00"));
--- src/main/java/com/HenriqueMundim/github/com/orange_app_api/infra/security/SecurityFilter.java
+++ src/main/java/com/HenriqueMundim/github/com/orange_app_api/infra/security/SecurityFilter.java
@@ -1,9 +1,11 @@
 package com.HenriqueMundim.github.com.orange_app_api.infra.security;
 
+import com.HenriqueMundim.github.com.orange_app_api.domain.services.token.TokenService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -13,9 +15,23 @@ import java.io.IOException;
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
 
+    private final TokenService tokenService;
+
+    @Autowired
+    public SecurityFilter(TokenService tokenService) {
+        this.tokenService = tokenService;
+    }
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         try {
+            String token = recoverToken(request);
+
+            if (token != null) {
+                String username = this.tokenService.validateToken(token);
+            }
+
+
         } catch (Exception exception) {
             System.out.println(exception.getMessage());
         }
