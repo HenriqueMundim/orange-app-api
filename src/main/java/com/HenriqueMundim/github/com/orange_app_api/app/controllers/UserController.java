5f9639c58e47f670ea20c0964e7f11c49100f91c
Henrique Matos <henriquemundimmatos@gmail.com>
2024-08-15 13:36:08 -0300

    Get user info Resource
    * feat: User controller created
    
    * feat: Get user info resource
    Create new endpoint to front retrieve user's information after login

--- src/main/java/com/HenriqueMundim/github/com/orange_app_api/app/controllers/UserController.java
+++ src/main/java/com/HenriqueMundim/github/com/orange_app_api/app/controllers/UserController.java
@@ -1,4 +1,29 @@
 package com.HenriqueMundim.github.com.orange_app_api.app.controllers;
 
+import com.HenriqueMundim.github.com.orange_app_api.domain.services.user.UserService;
+import com.HenriqueMundim.github.com.orange_app_api.infra.dto.OutputUserDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "users")
 public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public ResponseEntity<OutputUserDTO> getUserInfo(@AuthenticationPrincipal UserDetails userDetails) {
+        OutputUserDTO userInfo = userService.getUserByUsername(userDetails.getUsername());
+
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(userInfo);
+    }
 }
---
+++ src/main/java/com/HenriqueMundim/github/com/orange_app_api/domain/services/user/UserService.java
@@ -0,0 +1,25 @@
+package com.HenriqueMundim.github.com.orange_app_api.domain.services.user;
+
+import com.HenriqueMundim.github.com.orange_app_api.domain.entities.User;
+import com.HenriqueMundim.github.com.orange_app_api.infra.dto.OutputUserDTO;
+import com.HenriqueMundim.github.com.orange_app_api.infra.mapper.UserMapper;
+import com.HenriqueMundim.github.com.orange_app_api.infra.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public OutputUserDTO getUserByUsername(String username) {
+        User user = this.userRepository.findByUsername(username);
+
+        return UserMapper.toDomain(user);
+    }
+}
