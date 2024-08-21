1383cf1d7938bf6068a7f0dcd9d4b9c74b61ad46
Henrique Matos <henriquemundimmatos@gmail.com>
2024-08-21 13:59:26 -0300

    feat: Project essential methods Create getters, setters and equals hash code methods

--- src/main/java/com/HenriqueMundim/github/com/orange_app_api/domain/entities/Project.java
+++ src/main/java/com/HenriqueMundim/github/com/orange_app_api/domain/entities/Project.java
@@ -1,5 +1,7 @@
 package com.HenriqueMundim.github.com.orange_app_api.domain.entities;
 
+import java.util.Objects;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -34,6 +36,8 @@ public class Project {
 	@Column(name = "user_id", nullable = false)
 	private User author;
 	
+	public Project() {}
+	
 	public Project(Integer id, String title, String link, String description, String imageUrl) {
 		this.id = id;
 		this.title = title;
@@ -41,6 +45,70 @@ public class Project {
 		this.description = description;
 		this.imageUrl = imageUrl;
 	}
-	
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getLink() {
+		return link;
+	}
+
+	public void setLink(String link) {
+		this.link = link;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+
+	public User getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(User author) {
+		this.author = author;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(author, id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Project other = (Project) obj;
+		return Objects.equals(author, other.author) && Objects.equals(id, other.id);
+	}
 	
 }
