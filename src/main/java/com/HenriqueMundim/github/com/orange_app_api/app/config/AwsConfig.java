e2cb5403269ba569dfa25cc3886ab3be97362cf3
Henrique Matos <henriquemundimmatos@gmail.com>
2024-08-26 13:55:39 -0300

    Aws s3 bucket  service
    * feat: aws s3 dependency
    
    * feat: Amazon S3 configuration
    
    * feat: Amazon S3 service
    Create a method to generate presigned url
    
    * feat: amazon s3 bucket presigned url endpoint

---
+++ src/main/java/com/HenriqueMundim/github/com/orange_app_api/app/config/AwsConfig.java
@@ -0,0 +1,34 @@
+package com.HenriqueMundim.github.com.orange_app_api.app.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+
+@Configuration
+public class AwsConfig {
+	
+	@Value("${aws.access.key.id}")
+	private String accessKeyId;
+	
+	@Value("${aws.access.key.secret}")
+	private String accessKeySecret;
+	
+	@Value("${aws.region.name}")
+	private String region;
+
+    @Bean
+    AmazonS3 amazonS3() {
+		BasicAWSCredentials credentials = new BasicAWSCredentials(accessKeyId, accessKeySecret);
+		
+		return AmazonS3ClientBuilder.standard()
+				.withRegion(region)
+				.withCredentials(new AWSStaticCredentialsProvider(credentials))
+				.build();
+	}
+
+}
