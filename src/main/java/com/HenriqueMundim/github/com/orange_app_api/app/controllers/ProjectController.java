c6dbbebf657be484ca510c9e0314a3980bc566a1
Henrique Matos <henriquemundimmatos@gmail.com>
2024-08-27 13:54:13 -0300

    feat: Save project endpoint Create in project controller a new endpoint to register projects

--- src/main/java/com/HenriqueMundim/github/com/orange_app_api/app/controllers/ProjectController.java
+++ src/main/java/com/HenriqueMundim/github/com/orange_app_api/app/controllers/ProjectController.java
@@ -4,11 +4,15 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.HenriqueMundim.github.com.orange_app_api.domain.services.project.ProjectService;
+import com.HenriqueMundim.github.com.orange_app_api.infra.dto.CreateProjectDTO;
+import com.HenriqueMundim.github.com.orange_app_api.infra.dto.OutputProjectDTO;
 import com.HenriqueMundim.github.com.orange_app_api.infra.dto.UsersProjectDTO;
 
 @RestController
@@ -28,6 +32,11 @@ public class ProjectController {
 		@RequestParam(defaultValue = "9") Integer size
 	) {
 		return ResponseEntity.status(HttpStatus.OK).body(this.projectService.findAllByUser(id, page, size));
+	}
+	
+	@PostMapping
+	public ResponseEntity<OutputProjectDTO> RegisterProject(@RequestBody CreateProjectDTO project) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(this.projectService.save(project));
 	}
 }
    
